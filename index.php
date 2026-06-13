0e04739fc22a3809cec4c7eaa36f9d4772e2cf27
Rayfess <153087368+Rayfess@users.noreply.github.com>
2026-06-13 11:31:20 +0700

    feat: adding basic homepage

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+require_once("config.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vanila Basic Crud PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">VanCRUD</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </div>
+                <div class="navbar-nav">
+                    <a class="nav-link" href="login.php">Login</a>
+                    <a class="nav-link" href="register.php">Register</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Hero Section -->
+    <div class="container mt-5 text-center">
+        <div class="p-5 mb-4 bg-light rounded-3">
+            <h1 class="fs-1 fw-bold">Welcome To Our Sentences</h1>
+            <p class="col-md-8 fs-5 mx-auto">We'll wait your attendance here lets make it out</p>
+            <button class="btn btn-primary btn-lg mt-3" type="button">Learn More</button>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
