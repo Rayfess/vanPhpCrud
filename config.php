37d46a84a80f718ef926b9c0033668d86065d11d
Rayfess <153087368+Rayfess@users.noreply.github.com>
2026-06-14 14:33:08 +0700

    feat: adding dashboard(read) and config to work with it

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$host = "127.0.0.1";
+$dbname = "vancrud_db";
+$user = "root";
+$pass = "";
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage());
+}
+?>
